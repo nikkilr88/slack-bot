751008fc0a16ddee4dc0ee295c7ffbc2ff45a8c1
Nicole <nicole.sattler@live.com>
2017-12-30 14:32:26 -0600

    Fix formatting

--- helpers/functions.js
+++ helpers/functions.js
@@ -1,7 +1,6 @@
 const request = require('request');
 const { API_KEY } = require('../config/keys');
 
-
 exports.getWeather = (zip) => {
     let url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&units=imperial&appid=${API_KEY}`;
 
