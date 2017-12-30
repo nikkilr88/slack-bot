e017babfae56efaed76963a16fde23b7ade91dd9
Nicole <nicole.sattler@live.com>
2017-12-30 15:54:58 -0600

    Add additional controller.hears, clean up code

--- config/bot-config.js
+++ config/bot-config.js
@@ -9,18 +9,29 @@ module.exports = () => {
         token: SLACK_TOKEN
     });
 
-    //Listen for key words
+    //Listen for weather
     controller.hears(['weather ([0-9]{5})'], ['ambient'], (bot, message) => {
         controller.log('Slack message received');
+        
         let zip = message.match[1];
-
+        
         getWeather(zip)
         .then(data => {
-            bot.reply(message, `Forecast for ${data.name}: ${data.weather[0].main}, ${Math.round(data.main.temp)}°F`);
+            bot.reply(message, `Forecast for ${data.name}: ${data.weather[0].main}, ${data.main.temp}°F`);
         })
         .catch(err =>{
             bot.reply(message, err);
-        }) 
+        });
+    });
+
+    //Listen for other commands
+    controller.hears(['weather', 'help'], ['ambient'], (bot, message) => {        
+        if(message.text === 'weather') {
+            bot.reply(message, 'Did you want the weather? Enter <weather zipcode>.');
+        }
+        else {
+            bot.reply(message, 'I\'m just a bot. If you really need help, maybe you should call a fellow human. \n\n If you want your weather forecast, enter <weather zipcode>.');
+        }
     });
 
     // Use RTM
@@ -30,5 +41,4 @@ module.exports = () => {
         }
         controller.log('Slack connection established.');
     });
-    
 }
