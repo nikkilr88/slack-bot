1faeddac6f47b92c6a04a7e710ec7b798c1069ec
Nicole <nicole.sattler@live.com>
2018-01-02 13:45:52 -0600

    Add getUsers

--- helpers/functions.js
+++ helpers/functions.js
@@ -1,5 +1,5 @@
 const request = require('request');
-const { API_KEY } = require('../config/keys');
+const { API_KEY, SLACK_TOKEN } = require('../config/keys');
 
 exports.getWeather = (zip) => {
     let url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&units=imperial&appid=${API_KEY}`;
@@ -9,6 +9,22 @@ exports.getWeather = (zip) => {
             if (!error && response.statusCode === 200) {
                 let data = JSON.parse(body);
                 resolve(data);
+            }
+            else {
+                reject('Something went wrong.')
+            }
+        });
+    });
+}
+
+exports.getUsers = () => {
+    let url = `https://slack.com/api/users.list?token=${SLACK_TOKEN}`;
+
+    return new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                let users = JSON.parse(body);
+                resolve(users);
             }
             else {
                 reject('Something went wrong.')
